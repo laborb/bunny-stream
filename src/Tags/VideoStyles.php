ab315ebfb60e18fd12906f1b2b8af12c33db0b86
jf <jf@laborb.de>
2023-08-30 14:03:36 +0200

    Add compiled assets

--- src/Tags/VideoStyles.php
+++ src/Tags/VideoStyles.php
@@ -7,10 +7,10 @@ class VideoStyles extends \Statamic\Tags\Tags
     protected static $handle = 'video_styles';
 
     public function css() {
-        return '<link href="https://laborb.b-cdn.net/bunny-stream-assets/videojs.css" rel="stylesheet" />';
+        return '<link href="https://cdn.laborb.de/bunny-stream-assets/videojs.css" rel="stylesheet" />';
     }
 
     public function js() {
-       return '<script src="https://laborb.b-cdn.net/bunny-stream-assets/video.min.js"></script>';
+       return '<script src="https://cdn.laborb.de/bunny-stream-assets/video.min.js"></script>';
     }
 }

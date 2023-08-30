609af0fc9fdde8adc19ea1ed057450979642ac5b
jf <jf@laborb.de>
2023-08-30 12:10:29 +0200

    Update config

--- config/bunny.php
+++ config/bunny.php
@@ -6,7 +6,7 @@
  */
 
 return [
-    'libraryId' => 151629,
-    'apiKey' => '807bfbc1-72ce-4e98-854e5df18a0b-c6ea-472a',
-    'hostname' => 'vz-6d840a4d-dfd.b-cdn.net',
+    'libraryId' => null,
+    'apiKey' => '',
+    'hostname' => '',
 ];

a8d5a2d2ddc28be53a77cfbda0b9c007590eb455
jf <jf@laborb.de>
2024-02-23 11:51:56 +0100

    Update video.js to latest 8.11.4

--- src/Tags/VideoStyles.php
+++ src/Tags/VideoStyles.php
@@ -1,16 +1,18 @@
 <?php
 
 namespace Laborb\BunnyStream\Tags;
- 
+
 class VideoStyles extends \Statamic\Tags\Tags
 {
     protected static $handle = 'video_styles';
 
-    public function css() {
-        return '<link href="https://cdn.laborb.de/bunny-stream-assets/videojs.css" rel="stylesheet" />';
+    public function css()
+    {
+        return '<link href="https://cdn.laborb.de/bunny-stream-assets/videojs-8-11-4.min.css" rel="stylesheet" />';
     }
 
-    public function js() {
-       return '<script src="https://cdn.laborb.de/bunny-stream-assets/video.min.js"></script>';
+    public function js()
+    {
+        return '<script src="https://cdn.laborb.de/bunny-stream-assets/video-8-11-4.min.js"></script>';
     }
 }
